3e3dc3b3bccfcd013cb993656d4d842b20930a56
Nicoaedhel <nicoa.cn@gmail.com>
2015-04-23 11:32:41 +0200

    Ajout Factory Attachment

---
+++ client/assets/js/factories/attachments.js
@@ -0,0 +1,27 @@
+app.factory('AttachmentsFactory', function($http, $q) { 
+	var factory = {
+		Attachment : false,
+		addAttachment: function(depot, data) { 
+			var deferred = $q.defer();
+			$http({
+
+                    url: depot+'resource/attachment/',
+                    data: data,
+                    method: 'POST',
+                    headers : {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+
+                })
+				.success(function(data, status){
+					if(status == "204"){
+						deferred.reject('Erreur : Pas de contenu');
+					}else{
+						deferred.resolve('Film ajouté');
+					}
+				}).error(function(data, status){
+					deferred.reject('Erreur : impossible d\'ajouter le film');
+				})
+				return deferred.promise;
+		}
+	}
+	return factory;
+})
