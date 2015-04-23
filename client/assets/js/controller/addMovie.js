825f4a3bb970cab8a79865b38f01a3204d677ce4
Nicoaedhel <nicoa.cn@gmail.com>
2015-04-23 11:34:55 +0200

    Debut de fonction d'ajout de ressource

--- client/assets/js/controller/addMovie.js
+++ client/assets/js/controller/addMovie.js
@@ -1,8 +1,26 @@
-app.controller('AddMovieController', function ($scope, $rootScope, MoviesFactory) {
+app.controller('AddMovieController', function ($scope, $rootScope, AttachmentsFactory) {
   $rootScope.title = "Ajout d'un film";
   $scope.Tab1 = true;
   $scope.Tab2 = false;
   $scope.Tab3 = false;
   $scope.Tab4 = false;
   $scope.Tab5 = false;
+  $scope.submitMovie = function() {
+  		$scope.attachment = 
+		      {
+		      	_api_key_user : $rootScope.client_depots[0].user_key,
+		      	_api_key_password : $rootScope.client_depots[0].user_password,
+		      	_api_key_access : $rootScope.client_depots[0].depot_key_password,
+		      	_api_data : 
+		      	{
+      				data_type : $scope.movie.images_from_movie.filetype,
+      				data : $scope.movie.images_from_movie.base64
+		      	}
+		      };
+    	$scope.results = AttachmentsFactory.addAttachment($rootScope.client_depots[0].href,$scope.attachment).then(function(msg){
+			alert(msg);
+			}, function(msg){
+				alert(msg);
+			})
+    }
 });
