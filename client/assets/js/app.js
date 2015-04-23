dfbc419a743a89015165baa01f4ce3a049eec91f
Nicoaedhel <nicoa.cn@gmail.com>
2015-04-23 11:34:14 +0200

    Ajout directive pour l'encode 64
    Au niveau des formulaires d’ajout de ressource

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -18,12 +18,52 @@ app.controller("AppController", function($scope,$rootScope) {
     [
       {
         href : 'http://localhost:8888/codb-depot/codb-depot-file/',
-        depot_key_password : '',
-        user_key : '',
-        user_password : ''
+        depot_key_password : '552ce3b328a312.98946512',
+        user_key : '1234',
+        user_password : '1234'
       },
     ];
 });
+
+app.directive('baseSixtyFourInput', ['$window', function ($window) {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function (scope, elem, attrs, ngModel) {
+      var fileObject = {};
+
+      scope.readerOnload = function(e){
+        var base64 = _arrayBufferToBase64(e.target.result);
+        fileObject.base64 = base64;
+        scope.$apply(function(){
+          ngModel.$setViewValue(fileObject);
+        });
+      };
+
+      var reader = new FileReader();
+      reader.onload = scope.readerOnload;
+
+      elem.on('change', function() {
+        var file = elem[0].files[0];
+        fileObject.filetype = file.type;
+        fileObject.filename = file.name;
+        fileObject.filesize = file.size;
+        reader.readAsArrayBuffer(file);
+      });
+
+      //http://stackoverflow.com/questions/9267899/arraybuffer-to-base64-encoded-string
+      function _arrayBufferToBase64( buffer ) {
+        var binary = '';
+        var bytes = new Uint8Array( buffer );
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode( bytes[ i ] );
+        }
+        return $window.btoa( binary );
+      }
+    }
+  };
+}]);
 
 app.filter('isempty', function() {
     return function(input, replaceText) {
@@ -31,6 +71,8 @@ app.filter('isempty', function() {
         return replaceText;
     }
 });
+
+
 
 function config($urlProvider, $locationProvider) {
 $urlProvider.otherwise('/');
