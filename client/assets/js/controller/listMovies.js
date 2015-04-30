284cf725e32dd40480edf37edf8e1b485cec62ac
Nicoaedhel <nicoa.cn@gmail.com>
2015-04-30 16:00:03 +0200

    Ajout gif loader

--- client/assets/js/controller/listMovies.js
+++ client/assets/js/controller/listMovies.js
@@ -2,8 +2,10 @@ app.controller('ListMoviesController', function ($scope, $rootScope, MoviesFacto
   $rootScope.title = "Liste des films";
   $scope.OptionListCard = true;
   $scope.OptionListList = false;
+  $scope.Loader = true;
   $scope.resources = MoviesFactory.getMovies($rootScope.client_depots[0].href).then(function(data){
     $scope.resources = data;
+    $scope.Loader = false;
   }, function(msg){
     FoundationApi.publish('notification-panel', { title: 'Erreur :(', content: msg, color: 'alert' });
   });
