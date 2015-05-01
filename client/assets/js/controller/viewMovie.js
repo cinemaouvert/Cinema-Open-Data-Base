bdcbca6f4de7e42a930317af67b8ea1dd641dfde
Nicoaedhel <nicoa.cn@gmail.com>
2015-05-01 20:47:48 +0200

    Ajout gestion option
    sur une fiche film .

--- client/assets/js/controller/viewMovie.js
+++ client/assets/js/controller/viewMovie.js
@@ -1,6 +1,16 @@
 app.controller('ViewMovieController', function ($scope, $rootScope, MoviesFactory, $stateParams, FoundationApi, $location) {
+
   $scope.resource = MoviesFactory.getMovie($stateParams.depot, $stateParams.id).then(function(data){
     $scope.resource = data;
+    if ($scope.resource._api_key_user == $rootScope.client_depots[0].user_key) {
+    $scope.option = true;
+    $scope.optionEdit = true;
+    $scope.optionDelete = true;
+    }else{
+      $scope.option = false;
+      $scope.optionEdit = false;
+      $scope.optionDelete = false;
+    }
   }, function(msg){
     FoundationApi.publish('notification-fail', {
                         title: 'Erreur :(',
@@ -9,11 +19,15 @@ app.controller('ViewMovieController', function ($scope, $rootScope, MoviesFactor
                     });
     $location.path("ListMoviesController");
   });
+
   $rootScope.title = 'Consultation d\'un film';
   $scope.Tab1 = true;
   $scope.Tab2 = false;
   $scope.Tab3 = false;
   $scope.Tab4 = false;
   $scope.Tab5 = false;
+
+  
+  
 });
 
